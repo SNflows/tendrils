f0dea37e290c87b7519c707bda9f1d5c057d3290
Emir <ek2660@gmail.com>
2022-02-16 00:48:59 +0100

    add config copying and setting options

--- tendrils/utils/__init__.py
+++ tendrils/utils/__init__.py
@@ -2,7 +2,7 @@
 This subpackage contains utility functions for the Tendrils API.
 """
 
-from .config import load_config, get_api_token
+from .config import *
 from .files import get_filehash
 from .urls import urls, urls_from_config, get_request, post_request
 from .time import resolve_date_iso
